3d1eab557a818b7b049cca916be4da9984ca22ad
Aaron Ogle <aaron@geekgonecrazy.com>
2013-06-21 11:29:47 -0500

    Added my changed code.

--- run_server.js
+++ run_server.js
@@ -1,27 +1,33 @@
+// Code forked from: rabc/node-downloader
 var http = require('http');
 var nodeDownloader = require('./lib/node_download.js');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	var requested_url = req.url.substr(1);
 
-	var url = require('url').parse(req.url, true);
- 
-	res.write(url.query.file);
+ 	if (typeof requested_url == 'undefiend' || requested_url.length < 1 || requested_url == 'favicon.ico') {
+ 		res.end('Please provide a url to download.  Ex: http://127.0.0.1:8124/http://ipv4.download.thinkbroadband.com/10MB.zip');
+ 	} else {
+ 		var download = new nodeDownloader.NodeDownloader();
 
-	var download = new nodeDownloader.NodeDownloader();
+		download.setDirToSave('./');
+		download.downloadFile(req.url.substr(1));
+		// Alternatively download.downloadFile(req.url.substr(1), 'filename.zip');
+		
+		download.eventEmitter.on('progress', function(percent, speed) {
+			console.log('Percent: ' + percent + ' / Speed: ' + speed);
+		});
 
-	download.setDirToSave('/Users/borelli/Projetos/Web/node.js/downloads/');
-	download.downloadFile(url.query.file);
-	
-	download.eventEmitter.on('progress', function(percent, speed) {
-		console.log('percent: ' + percent);
-		console.log('speed: ' + speed);
-	});
-	
-	// just to stop
-	setTimeout(function() {
-						download.stopDownload();
-					}, 5000);
+		download.eventEmitter.on('end', function(code) {
+			if (code == 0) {
+				console.log('Downloaded ' + requested_url + ' Successfully.');
+			} else {
+				console.log('Download did not succeed.  Exited with code:' + code);
+			}
+			
+			res.end('URL: ' + requested_url + ' Downloaded');
+		});
+ 	}
 
   
 }).listen(8124, "127.0.0.1");
