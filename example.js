efa70f4abc01435b505f1504d6378c7e967df4fe
Aaron Ogle <AaronOgle@users.noreply.github.com>
2013-09-13 10:41:49 -0500

    Update example.js

--- example.js
+++ example.js
@@ -11,7 +11,7 @@ http.createServer(function (req, res) {
  		var download = new Downloader.CurlDownloader();
 
 		download.setDirToSave('~/Desktop/abc/123/456/789/qrst/');
-		download.downloadFile('https://dvtools.com/fmi/xml/cnt/data.cnt?-db=Dv_DataBase_2008_1v3%20Recovered&-lay=QtLi%20Simple%20Draw%20A&-recid=33572&-field=Li_CreatedIn_AiPsd_Graphic(1)', 'file.ai');
+		download.downloadFile('http://127.0.0.1:8124/http://ipv4.download.thinkbroadband.com/10MB.zip', '10MB.zip');
 		// Alternatively download.downloadFile(req.url.substr(1), 'filename.zip');
 		
 		download.eventEmitter.on('progress', function(percent, speed) {
